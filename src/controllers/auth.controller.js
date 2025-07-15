40b403d1ed938890e9301ef6200e459e39a7faa6
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-15 13:26:35 +0700

    feat: add handler login and register in auth flow

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,61 @@
+const { constants: http } = require('http2')
+
+const userModels = require('../models/user.models')
+
+/**
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @returns 
+ */
+exports.register = function (req, res) {
+  const { email, password, confirmPassword } = req.body
+
+  if (userModels.getUserByEmail(email) !== undefined) {
+    return res.status(http.HTTP_STATUS_BAD_REQUEST).json({
+      success: false,
+      message: "email already exists!"
+    })
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(http.HTTP_STATUS_BAD_REQUEST).json({
+      success: false,
+      message: "confirm password do not match"
+    })
+  }
+
+  const allUsers = userModels.getAllUsers
+  const newUser = {
+    id: allUsers.length + 1,
+    email,
+    password
+  }
+
+  userModels.createUser(newUser)
+
+  console.log(allUsers)
+
+  return res.status(http.HTTP_STATUS_CREATED).json({
+    success: true,
+    message: 'register success!',
+    users: newUser
+  })
+}
+
+
+/**
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @returns 
+ */
+exports.login = function (req, res) {
+  const { email, password } = req.body
+
+
+  return res.status(http.HTTP_STATUS_UNAUTHORIZED).json({
+    success: true,
+    message: 'login success!'
+  })
+}
