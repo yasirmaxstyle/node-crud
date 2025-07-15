3ac80a16f07379f3a5db8d67f794ffece7938433
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-15 17:14:10 +0700

    feat: add pagination when getting all users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -36,16 +36,37 @@ exports.deleteUser = function (req, res) {
   })
 }
 
+/**
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @returns 
+ */
 exports.getUsers = function (req, res) {
-  const users = userModels.getAllUsers()
+  const { search, page, limit } = req.query
+  const users = userModels.getAllUsers(search)
+
+  const TOTAL = users.length
+  const LIMIT = limit || 3
+  const OFFSET = (page - 1) * LIMIT || 0
+  const totalPage = Math.floor((TOTAL + LIMIT - 1) / LIMIT)
 
   return res.status(http.HTTP_STATUS_OK).json({
     success: true,
     message: 'data retrieved successfully!',
-    data: users
+    total: TOTAL,
+    limit: LIMIT,
+    totalPage,
+    results: users.slice(OFFSET, LIMIT + OFFSET)
   })
 }
 
+/**
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @returns 
+ */
 exports.getUserById = function (req, res) {
   const { id } = req.params
 
