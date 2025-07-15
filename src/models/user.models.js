4de9ef33d69a4865454f2f451696c3dc20460423
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-15 15:32:05 +0700

    feat: add models for update and delete users

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,4 @@
-const users = []
+let users = []
 
 exports.createUser = (user) => {
   users.push(user)
@@ -10,4 +10,22 @@ exports.getUserByEmail = (email) => {
 
 exports.getAllUsers = () => {
   return users
+}
+
+exports.getUserById = (id) => {
+  return users.find(user => user.id === id)
+}
+
+exports.updateUser = (id, newData) => {
+  let idx = users.findIndex(user => user.id === id)
+  if (idx !== -1) {
+    users[idx] = { ...users[idx], ...newData }
+  }
+}
+
+exports.deleteUser = (id) => {
+  let user = users.find(user => user.id === id)
+  if (user) {
+    users = users.filter(user => user.id !== id)
+  }
 }
