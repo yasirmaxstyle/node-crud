c6ab21c4a06c9853a93711bc3bc96402ca230360
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-17 13:11:22 +0700

    feat: add path create user, enable upload photo using multer

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,9 +1,25 @@
 const userRouter = require('express').Router()
+const path = require("node:path")
+const multer = require("multer")
+const { v4: uuid } = require("uuid")
 
 const userController = require('../controllers/user.controller')
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join("uploads", "profile-picture"))
+  },
+  filename: (req, file, cb) => {
+    const filename = file.originalname;
+    const ext = filename.split(".")[1]
+    const savedFile = `${uuid()}.${ext}`
+    cb(null, savedFile)
+  }
+})
+const profilePicture = multer({ storage })
 
 userRouter.get('/', userController.getUsers)
 userRouter.get('/:id', userController.getUserById)
+userRouter.post('/', profilePicture.single("picture"), userController.createUser)
 userRouter.patch('/:id', userController.editUser)
 userRouter.delete('/:id', userController.deleteUser)
 
